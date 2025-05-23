38adb821a92887f6afaa2613e9222bb0377779da
FaysalDurlov <foysaldurlov@gmail.com>
2025-05-23 19:00:21 +0600

    Updated_Notes_Class

---
+++ JS_Class/script.js
@@ -0,0 +1,11 @@
+// class
+
+function Car(comapany, model,year,color){
+    this.comapany = comapany,
+    this.model = model,
+    this.year = year,
+    this.color = color
+}
+let car1 = new Car('Toyota','Supra-450',2018,'Black');
+console.log(car1.comapany);
+console.log(car1);
--- zNotes/CheatSheet.js
+++ zNotes/CheatSheet.js
@@ -328,3 +328,17 @@ document.body.addEventListener('keydown',(event)=>{ // here we can get the event
 });                          // check html Notes
 
 // For Dynamics. Let say Todo Delete Button we have to set html first then we can set addeventlistener
+
+
+/*============================= Class Create ================================================================================================================================================================*/
+
+
+function Car(comapany, model,year,color){
+    this.comapany = comapany,
+    this.model = model,
+    this.year = year,
+    this.color = color
+}
+let car1 = new Car('Toyota','Supra-450',2018,'Black');
+console.log(car1.comapany);
+console.log(car1);
